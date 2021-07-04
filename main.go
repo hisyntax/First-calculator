6a11ae68485f4838ec13e32507b0137c68b69378
hisyntax <hisyntax0@gmail.com>
2021-07-04 12:41:24 +0100

    I have added another feature to the calculator

--- main.go
+++ main.go
@@ -2,56 +2,55 @@ package main
 
 import "fmt"
 
+func add(x, y int) int {
+	return x + y
+}
+
+func sub(x, y int) int {
+	return x - y
+}
+
+func mul(x, y int) int {
+	return x * y
+}
+
+func div(x, y int) int {
+	return x / y
+}
+
 func main() {
 	defer func() {
-		r := recover(); r != nil {
-			fmt.Println("Cannot be divided by zero...")
-
+		if r := recover(); r != nil {
+			fmt.Println("Cannot divide a number by zero: ")
 			main()
 		}
 	}()
 
-	functions := map[string]func(int, int) int {
-		"addition": add,
-		"subtraction": sub,
-		"multiplication": multiply,
-		"division": mul,
+	functions := map[string]func(int, int) int{
+		"add": add,
+		"sub": sub,
+		"mul": mul,
+		"div": div,
 	}
-
 	var currentNumber int
-	fmt.Println("Enter a number: ")
+	fmt.Println("Enter a number")
 	fmt.Scanln(&currentNumber)
 
 	for true {
 		var functionName string
-		fmt.Println("What funcion do you want to use: ")
+		var number int
+		fmt.Println("What function do you want to use e.g mul = multiply, add = addition, div = division, sub = subtract ... and done = done to end the program")
 		fmt.Scanln(&functionName)
 		if functionName == "done" {
 			break
 		}
-		var number int
-		fmt.Println("Enter a number: ")
+
+		fmt.Println(functionName, currentNumber, "by...")
 		fmt.Scanln(&number)
 
-		currentNumber = functions[functionName](currentNumber,number)
+		currentNumber := functions[functionName](currentNumber, number)
 		fmt.Println("Your result is: ")
 		fmt.Println(currentNumber)
-		
 	}
-}
 
-func addition(x, y int) int {
-	return x + y
-}
-
-func subtraction(x, y int) int {
-	return x - y
-}
-
-func multiplication(x, y int) int {
-	return x * y
-}
-
-func division(x, y int) int {
-	return x / y
 }
